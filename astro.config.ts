9e191d1ac5062f8958d69ec711097035bf4f23c0
Tony Norlin <tony.norlin@localdomain.se>
2026-01-02 19:27:06 +0100

    Prettier
    Signed-off-by: Tony Norlin <tony.norlin@localdomain.se>

--- astro.config.ts
+++ astro.config.ts
@@ -6,39 +6,40 @@ import remarkCollapse from "remark-collapse";
 import { SITE } from "./src/config";
 
 import expressiveCode from "astro-expressive-code";
-import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
-
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
 import react from "@astrojs/react";
 
-
 import mdx from "@astrojs/mdx";
-
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [sitemap({
-    filter: page => SITE.showArchives || !page.endsWith("/archives"),
-  }), expressiveCode({
-    // You can set configuration options here
-    themes: ['material-theme-darker', 'github-light'],
-    styleOverrides: {
-      // You can also override styles
-      borderRadius: '0.5rem',
-      frames: {
+  integrations: [
+    sitemap({
+      filter: page => SITE.showArchives || !page.endsWith("/archives"),
+    }),
+    expressiveCode({
+      // You can set configuration options here
+      themes: ["material-theme-darker", "github-light"],
+      styleOverrides: {
+        // You can also override styles
+        borderRadius: "0.5rem",
+        frames: {},
       },
-    },
-    defaultProps: {
+      defaultProps: {
         // Enable word wrap by default
         wrap: true,
         // Disable wrapped line indentation for terminal languages
         overridesByLang: {
-          'bash,ps,sh': { preserveIndent: false },
+          "bash,ps,sh": { preserveIndent: false },
         },
       },
-    plugins: [pluginLineNumbers()],
-  }), react(), mdx()],
+      plugins: [pluginLineNumbers()],
+    }),
+    react(),
+    mdx(),
+  ],
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
   },
