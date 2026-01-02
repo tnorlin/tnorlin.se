011a66dd98b299ae8b75d346edb1689ed0baa780
Tony Norlin <tony.norlin@localdomain.se>
2026-01-02 18:40:45 +0100

    Revision
    Signed-off-by: Tony Norlin <tony.norlin@localdomain.se>

---
+++ src/utils/lifeFilter.ts
@@ -0,0 +1,10 @@
+import type { CollectionEntry } from "astro:content";
+import { SITE } from "@/config";
+
+const postFilter = ({ data }: CollectionEntry<"blog">) => {
+  const isPublishTimePassed =
+    Date.now() >
+    new Date(data.pubDatetime).getTime() - SITE.scheduledPostMargin;
+  return (!data.draft && data.category == "life") && (import.meta.env.DEV || isPublishTimePassed);
+};
+export default postFilter;
